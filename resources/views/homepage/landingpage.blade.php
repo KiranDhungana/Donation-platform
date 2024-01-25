5f05df5811fe022e737b4fc9689e5b6f4f83f3c9
roshancoder <pokhrelroshan224@gmail.com>
2024-01-25 23:40:26 +0545

    trending

--- resources/views/homepage/landingpage.blade.php
+++ resources/views/homepage/landingpage.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home</title>
    @include('globallink') 
-    <link rel="stylesheet" href="{{ asset('assets/csss/customcss.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/customcss.css') }}">
     </head>
 <body>
  <nav class="bg-transparent fixed min-w-full">
@@ -34,18 +34,18 @@
 
                 <div class="flex flex-1 items-center justify-between sm:items-stretch sm:justify-between ">
                     <div class="flex flex-shrink-0 items-center">
-                        <img class="h-10 w-10 rounded-full logo" src="./Images/logo.png" alt="Raise for good"">
+                        <img class="h-10 w-10 rounded-full logo" src="Images/logo.png" alt="Raise for good"">
                   </div>
                   <div class=" hidden sm:ml-6 sm:block">
                         <div class="flex space-x-4">
-                            <a href="#" class=" text-green rounded-md px-3 py-2 text-sm font-medium"
+                            <a href="#" class=" text-blue rounded-md px-3 py-2 text-sm font-medium"
                                 aria-current="page">Homepage</a>
                             <a href="#"
-                                class="text-gray-300  hover:text-green rounded-md px-3 py-2 text-sm font-medium">Campaign</a>
+                                class="text-gray-300  hover:text-blue rounded-md px-3 py-2 text-sm font-medium">Campaign</a>
                             <a href="#"
-                                class="text-gray-300  hover:text-green rounded-md px-3 py-2 text-sm font-medium">About</a>
+                                class="text-gray-300  hover:text-blue rounded-md px-3 py-2 text-sm font-medium">About</a>
                             <a href="#"
-                                class="text-gray-300  hover:text-green rounded-md px-3 py-2 text-sm font-medium">Contact</a>
+                                class="text-gray-300  hover:text-blue rounded-md px-3 py-2 text-sm font-medium">Contact</a>
                         </div>
                     </div>
 
@@ -78,7 +78,7 @@
     <div class="main_view flex justify-center w-auto items-center flex-col">
         <h1 class="main_heading text-white text-xl font-bold my-3 w-auto mx-auto text-center md:w-3/4 md:text-4xl">
             Donating isn't just about making a difference;</h1>
-        <h1 class="main_heading text-yellow text-xl font-bold  w-auto mx-auto text-center md:w-6/12 md:text-4xl">
+        <h1 class="main_heading text-blue text-xl font-bold  w-auto mx-auto text-center md:w-6/12 md:text-4xl">
             It's about being the difference.</h1>
 
         <p class="main_heading text-white text-xl mx-auto text-center my-3 w-full md:w-1/2">Lorem ipsum dolor sit amet
@@ -86,11 +86,43 @@
             exce</p>
 
         <div class="main_view_btns flex w-1/2 justify-center my-3">
-            <a href="" class="bg-green text-white rounded-md px-4 py-2 text-xl  mx-8">Donate</a>
+            <a href="" class="bg-blue text-white rounded-md px-4 py-2 text-xl  mx-8">Donate</a>
             <a href="" class="bg-yellow text-white rounded-md px-4 py-2 text-xl ">SignUp</a>
 
         </div>
     </div>
+
+    <div class="homepage_campaign bg-blue py-8">
+        <h1 class="container_heading py-3">Trending Campaign</h1>
+        <div class="campaign_gallery flex justify-center">
+            <div class="gallery_box m-3">
+                <img class="hover:opacity-50" src="https://www.fredhutch.org/en/news/center-news/2020/05/what-happens-when-cancer-patients-get-covid-19-/_jcr_content/root/responsivegrid/textimage/image.coreimg.jpeg/1638427334811/im6col-200528-cancer-covid19.jpeg" alt="">
+                <p class="text-white text-center">Cancer Patient</p>
+            </div>
+            <div class="gallery_wrapper flex flex-wrap w-1/2">
+                <div class="gallery_box w-fit  mt-3 mr-3">
+                    <img src="https://scx1.b-cdn.net/csz/news/800a/2016/survivalofqu.jpg" class="hover:opacity-50">
+                    <p class="text-white text-center">Earthquake Victim</p>
+                </div>
+                <div class="gallery_box w-fit  mt-3 mr-3">
+                    <img src="https://scx1.b-cdn.net/csz/news/800a/2016/survivalofqu.jpg" class="hover:opacity-50">
+                    <p class="text-white text-center">Earthquake Victim</p>
+                </div>
+                <div class="gallery_box w-fit  mt-3 mr-3">
+                    <img src="https://scx1.b-cdn.net/csz/news/800a/2016/survivalofqu.jpg" class="hover:opacity-50">
+                    <p class="text-white text-center">Earthquake Victim</p>
+                </div>
+                <div class="gallery_box w-fit  mt-3 mr-3">
+                    <img src="https://scx1.b-cdn.net/csz/news/800a/2016/survivalofqu.jpg" class="hover:opacity-50">
+                    <p class="text-white text-center">Earthquake Victim</p>
+                </div>
+              
+               
+            </div>
+        </div>
+    </div>
+
+
 
 
 
