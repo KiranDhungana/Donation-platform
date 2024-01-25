1c7a49d2077bac974a3b0ede8ecde70007113aec
KiranDhungana <kirandhungana570@gmail.com>
2024-01-25 21:51:49 +0545

    ok

--- resources/views/superadmin/homepage.blade.php
+++ resources/views/superadmin/homepage.blade.php
@@ -4,7 +4,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
  
-  <link rel="stylesheet" href="{{ asset('assets/csss/globallink.blade.php') }}">
+  <link rel="stylesheet" href="{{ asset('assets/css/globallink.blade.php') }}">
   <title>SuperAdmin</title>
   <style>
 
