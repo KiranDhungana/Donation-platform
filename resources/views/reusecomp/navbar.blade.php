15221aaf89c9fc1ec60bfc2d986def342592daf5
roshancoder <pokhrelroshan224@gmail.com>
2024-01-30 23:04:08 +0545

    minor

--- resources/views/reusecomp/navbar.blade.php
+++ resources/views/reusecomp/navbar.blade.php
@@ -7,66 +7,78 @@
 
 <nav class="bg-transparent  fixed w-full z-20 top-0 start-0">
   <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-  <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
+    <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
       <img src="{{ asset('assets/Images/logo.png') }}" class="h-8" alt="Flowbite Logo">
-     
-  </a>
-  <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+
+    </a>
+    <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
       <button type="button" class="text-white  
-      font-medium rounded-lg text-sm px-4 py-2 text-center" style="background: #234E70;" data-modal-target="authentication-modal" data-modal-toggle="authentication-modal">Login</button>
+      font-medium rounded-lg text-sm px-4 py-2 text-center" style="background: #234E70;"
+        data-modal-target="authentication-modal" data-modal-toggle="authentication-modal">Login</button>
       @if(Auth::user())
-<button id="dropdownUserAvatarButton" data-dropdown-toggle="dropdownAvatar" class="flex text-sm  rounded-full md:me-0 " type="button" style="margin-left:1rem;outline:none">
-<span class="sr-only">Open user menu</span>
-<img class="w-8 h-8 rounded-full " src="https://img.freepik.com/premium-photo/cartoon-game-avatar-logo-gaming-brand_902820-465.jpg" alt="user photo">
-</button>
-<div id="dropdownAvatar" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
-    <div class="px-4 py-3 text-sm text-gray-900 dark:text-white">
-      <div>{{Auth::user()->fname}}{{ Auth::user()->mname }} {{ Auth::user()->lname}}</div>
-      <div class="font-medium truncate">{{Auth::user()->email}}</div>
-    </div>
-    <ul class="py-2 text-sm text-gray-700" aria-labelledby="dropdownUserAvatarButton">
-      <li>
-        <a href="#" class="block px-4 py-2 hover:text-blue-600">Profile</a>
-      </li>
-      <li>
-        <a href="/setting/{{Auth::user()->id}}/{{Auth::user()->fname}}" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
-      </li>
-   
-    </ul>
-    <div class="py-2">
-      <a href="/logout" class="block px-4 py-2 text-sm text-gray-700 bg-blue dark:text-gray-200 dark:hover:text-white">Sign out</a>
-    </div>
-</div>
-@else
-<div>
-</div>
-    
-@endif
+      <button id="dropdownUserAvatarButton" data-dropdown-toggle="dropdownAvatar"
+        class="flex text-sm  rounded-full md:me-0 " type="button" style="margin-left:1rem;outline:none">
+        <span class="sr-only">Open user menu</span>
+        <img class="w-8 h-8 rounded-full "
+          src="https://img.freepik.com/premium-photo/cartoon-game-avatar-logo-gaming-brand_902820-465.jpg"
+          alt="user photo">
+      </button>
+      <div id="dropdownAvatar"
+        class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
+        <div class="px-4 py-3 text-sm text-gray-900 dark:text-white">
+          <div>{{Auth::user()->fname}}{{ Auth::user()->mname }} {{ Auth::user()->lname}}</div>
+          <div class="font-medium truncate text-white">{{Auth::user()->email}}</div>
+        </div>
+        <ul class="py-2 text-sm text-gray-700" aria-labelledby="dropdownUserAvatarButton">
+          <li>
+            <a href="#" class="block px-4 py-2 text-white">Profile</a>
+          </li>
+          <li>
+            <a href="/setting/{{Auth::user()->id}}/{{Auth::user()->fname}}"
+              class="block px-4 py-2 text-white">Settings</a>
+          </li>
+
+        </ul>
+        <div class="py-2">
+          <a href="/logout"
+            class="block px-4 py-2 text-sm text-gray-700 text-white">Sign out</a>
+        </div>
+      </div>
+      @else
+      <div>
+      </div>
+
+      @endif
 
 
 
-      <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
+      <button data-collapse-toggle="navbar-sticky" type="button"
+        class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+        aria-controls="navbar-sticky" aria-expanded="false">
         <span class="sr-only">Open main menu</span>
         <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+            d="M1 1h15M1 7h15M1 13h15" />
         </svg>
-    </button>
-  </div>
-  <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-    <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium  rounded-lg  md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0  ">
-      <li>
-        <a href="#" class="block py-2 px-3 text-white rounded md:p-0" aria-current="page">Home</a>
-      </li>
-      <li>
-        <a href="#" class="block py-2 px-3 text-gray-900 rounded  md:p-0 text-white dark:border-gray-700">Campaigns</a>
-      </li>
-      <li>
-        <a href="#" class="block py-2 px-3 text-gray-900 rounded  md:p-0 text-white dark:border-gray-700">About</a>
-      </li>
-      <li>
-        <a href="#" class="block py-2 px-3 text-gray-900 rounded  md:p-0 text-white dark:border-gray-700">Contact</a>
-      </li>
-    </ul>
-  </div>
+      </button>
+    </div>
+    <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+      <ul
+        class="flex flex-col p-4 md:p-0 mt-4 font-medium  rounded-lg  md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0  ">
+        <li>
+          <a href="#" class="block py-2 px-3 text-white rounded md:p-0" aria-current="page">Home</a>
+        </li>
+        <li>
+          <a href="#"
+            class="block py-2 px-3 text-gray-900 rounded  md:p-0 text-white dark:border-gray-700">Campaigns</a>
+        </li>
+        <li>
+          <a href="#" class="block py-2 px-3 text-gray-900 rounded  md:p-0 text-white dark:border-gray-700">About</a>
+        </li>
+        <li>
+          <a href="#" class="block py-2 px-3 text-gray-900 rounded  md:p-0 text-white dark:border-gray-700">Contact</a>
+        </li>
+      </ul>
+    </div>
   </div>
 </nav>
