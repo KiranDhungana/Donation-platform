4e6b4d64439d7cc0ef278f75e101ab88a738fb9c
KiranDhungana <kirandhungana570@gmail.com>
2024-02-03 09:31:22 +0545

    merge conflict

--- resources/views/reusecomp/navbar.blade.php
+++ resources/views/reusecomp/navbar.blade.php
@@ -14,16 +14,10 @@
     <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
       @if(!Auth::user())
       <button data-modal-target="authentication-modal" data-modal-toggle="authentication-modal" type="button"
-<<<<<<< HEAD
-        class="mx-3 text-white bg-blue hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</button>
-      <a href="campaign-form" class="border px-3 py-2 border-gray-800 rounded post_btn"><i class="fa-solid fa-plus mr-2"></i> Add
-        Post</a>
-=======
         class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</button>
 @else
 
 @endif
->>>>>>> 5b22ec0a54e4b47dc17241d8c622600837ff5a2a
       <!-- Dropdown menu -->
       @if (Auth::user())
 
