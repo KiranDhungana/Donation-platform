3917c56ecb464dfa8623c5bfe17aa470715fb0ab
roshancoder <pokhrelroshan224@gmail.com>
2024-02-10 14:10:32 +0545

    search

--- resources/views/homepage/campaignPost.blade.php
+++ resources/views/homepage/campaignPost.blade.php
@@ -9,6 +9,11 @@
     <link rel="stylesheet" href="{{ asset('assets/css/customcss.css') }}">
 
 </head>
+<style>
+    #searchResults{
+        display: none;
+    }
+</style>
 
 <body>
     <div class="page_container bg-blue ">
@@ -50,7 +55,7 @@
                         </svg>Search
                     </button>
                 </form>
-                <div id="searchResults"></div>
+             
 
             </div>
         </div>
@@ -98,11 +103,14 @@
                         <div class="saarch_heading">
                             <p class="text-xl text-white font-bold my-2">Location</p>
                         </div>
-                        <div class="search_items flex flex-col ">
-                            <a href="" class="text-white text-sm my-1">Butwal</a>
-                            <a href="" class="text-white text-sm my-1">Inhurwa</a>
-                            <a href="" class="text-white text-sm my-1">Khaireni</a>
-                            <a href="" class="text-white text-sm my-1">Kathmandu</a>
+                        <div class="search_items flex flex-col " id="filter_prov">
+                            <a href="" class="text-white text-sm my-1">Province No. 1</a>
+                            <a href="" class="text-white text-sm my-1">Madhesh</a>
+                            <a href="" class="text-white text-sm my-1">Bagmati</a>
+                            <a href="" class="text-white text-sm my-1">Gandaki</a>
+                            <a href="" class="text-white text-sm my-1">Lumbini</a>
+                            <a href="" class="text-white text-sm my-1">Karnali</a>
+                            <a href="" class="text-white text-sm my-1">Sudurpashchim</a>
 
                         </div>
                     </div>
@@ -110,13 +118,15 @@
 
 
                 </div>
+                <div id="searchResults" class="post_container w-full flex flex-row flex-wrap">
 
-                <div class="post_container w-full flex flex-col">
+                </div>
+                <div class="post_container w-full flex flex-col" id="default_data">
 
 
                     <div class="campaigns_card_container flex flex-wrap my-3 justify-center items-center">
-@foreach ($post as $item)
-    <div class="campaign_card m-5">
+                        @foreach ($post as $item)
+                        <div class="campaign_card m-5">
                             <div
                                 class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                                 <a href="#">
@@ -128,14 +138,15 @@
                                     <a href="#">
                                         <h5
                                             class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                            Help Needed ! 
-                                            </h5>
+                                            Help Needed !
+                                        </h5>
                                     </a>
                                     <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                                    
-                                        {{ \Illuminate\Support\Str::limit($item['description'], $limit = 200, $end = '...') }}
-                                        
-                                       
+
+                                        {{ \Illuminate\Support\Str::limit($item['description'], $limit = 200, $end =
+                                        '...') }}
+
+
                                     </p>
                                     <a href="/view-post/{{$item['id']}}"
                                         class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
@@ -159,9 +170,9 @@
                                 </div>
                             </div>
                         </div>
-@endforeach
-                        
-                      
+                        @endforeach
+
+
                     </div>
                 </div>
 
@@ -171,30 +182,79 @@
     </div>
 
     <script>
-    $('#searchForm').submit(function(e) {
-        debugger
-        e.preventDefault();
+        $('#searchForm').submit(function (e) {
+            debugger
+            e.preventDefault();
 
-        var query = $('#query').val();
+            var query = $('#query').val();
 
-        $.ajax({
-            type: 'GET',
-            url: '{{ route('search') }}',
-            data: { query: query },
-            success: function(response) {
-                $('#searchResults').empty();
+            $.ajax({
+                type: 'GET',
+                url: '{{ route('search') }}',
+                data: { query: query },
+                success: function (response) {
+                    $('#searchResults').empty();
+                    $('#searchResults').css('display', 'flex');
+                    $('#default_data').hide()
 
-                $.each(response, function(index, item) {
-                    console.log(response);
-                    $('#searchResults').append('<p>' + item.title + '</p>'); 
-                });
-            },
-            error: function(xhr) {
-                console.log(xhr.responseText);
-            }
+                    $.each(response, function (index, item) {
+                        console.log(response);
+                        $('#searchResults').append(
+
+                            `<div class="campaign_card m-5">
+                                <div
+                                    class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                                    <a href="#">
+                                        <img class="rounded-t-lg"
+                                            src="https://akm-img-a-in.tosshub.com/indiatoday/images/story/202401/japan-earthquake-044751828-16x9_0.jpg?VersionId=RBM6I1Flkjgb8On.fmy3IlKcXUMLAhNG&size=690:388"
+                                            alt="" />
+                                    </a>
+                                    <div class="p-5">
+                                        <a href="#">
+                                            <h5
+                                                class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                                                ${item.title}
+                                            </h5>
+                                        </a>
+                                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                                            ${item.description}
+                                          
+
+
+                                        </p>
+                                        <a href="/view-post/${item.id}"
+                                            class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                                            Details
+                                            <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true"
+                                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                                    stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                                            </svg>
+                                        </a>
+                                        <div class="views border-t py-2 mt-2 border-gray-500">
+                                            <div class="total_views w-1/2 flex">
+                                                <i class="fa-regular fa-eye mr-3"
+                                                    style="font-size: 12px; color: white;"><span
+                                                        class="text-sm ml-1">33</span></i>
+                                                <i class="fa-regular fa-heart" style="font-size: 12px; color: white;"><span
+                                                    class="text-sm ml-1"></span></i>
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>`
+                        );
+                    });
+                },
+                error: function (xhr) {
+                    console.log(xhr.responseText);
+                }
+            });
         });
-    });
-</script>
+    </script>
+
+
 
 </body>
 
