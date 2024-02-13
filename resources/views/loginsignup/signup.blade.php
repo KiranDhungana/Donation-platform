7164e48368a83c218786536d031a454e68740af3
roshancoder <pokhrelroshan224@gmail.com>
2024-02-13 10:04:43 +0545

    ui changed

--- resources/views/loginsignup/signup.blade.php
+++ resources/views/loginsignup/signup.blade.php
@@ -14,13 +14,13 @@
     @include('reusecomp/navbar')
 
     <div class="register_form_container">
-        <section class="bg-gray-900">
+        <section class="bg-yellow">
             <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto">
                 <div
-                    class="w-full bg-white rounded-lg shadow dark:border my-20 sm:w-1/2 xl:p-0 dark:bg-gray-800 dark:border-gray-700 ">
+                    class="w-full bg-white rounded-lg shadow dark:border my-20 sm:w-1/2 xl:p-0 ">
                     <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
                         <h1
-                            class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                            class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
                             Create and account
                         </h1>
                         <form action="{{ route('studentRegister') }}" method="post" class="space-y-4 md:space-y-6"
@@ -28,48 +28,48 @@
                             @csrf
                             <div>
                                 <label for="firstname"
-                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First
+                                    class="block mb-2 text-sm font-medium text-blue">First
                                     Name</label>
                                 <input type="text" name="firstname" id="firstname"
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                                     placeholder="First Name" required="">
                             </div>
                             <div class="flex justify-between">
                                 <div class="w-80">
                                     <label for="middlename"
-                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Middle Name
+                                        class="block mb-2 text-sm font-medium text-blue">Middle Name
                                     </label>
                                     <input type="text" name="middlename" id="middlename"
-                                        class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
                                         placeholder="Middle Name">
                                 </div>
 
                                 <div class="w-1/2">
                                     <label for="lastname"
-                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Last
+                                        class="block mb-2 text-sm font-medium text-blue">Last
                                         Name</label>
                                     <input type="text" name="lastname" id="lastname"
-                                        class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                                         placeholder="Last Name" required="">
                                 </div>
 
                             </div>
                             <div>
                                 <label for="email"
-                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
+                                    class="block mb-2 text-sm font-medium text-blue">Your
                                     email</label>
                                 <input type="email" name="email" id="email"
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
                                     placeholder="yourname@gmail.com" required>@error('email')
                                 <div id="emailHelp" style="color:red" class="form-text">{{ $message }}</div>
                                 @enderror
                             </div>
                             <div>
                                 <label for="phonenumber"
-                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Phone
+                                    class="block mb-2 text-sm font-medium text-blue">Phone
                                     Number</label>
                                 <input type="number" name="phonenumber" id="phonenumber"
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
                                     placeholder="Enter Phone Number" required> @error('phonenumber')
                                 <div id="emailHelp" style="color:red" class="form-text">{{ $message }}</div>
                                 @enderror
@@ -77,20 +77,20 @@
 
                             <div>
                                 <label for="password"
-                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                                    class="block mb-2 text-sm font-medium text-blue">Password</label>
                                 <input type="password" name="password" id="password" placeholder="••••••••"
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
                                     required=""> @error('password')
                                 <div id="emailHelp" style="color:red" class="form-text">{{ $message }}</div>
                                 @enderror
                             </div>
                             <div>
                                 <label for="confirm-password"
-                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm
+                                    class="block mb-2 text-sm font-medium text-blue">Confirm
                                     password</label>
                                 <input type="confirm-password" name="confirmed" id="confirm-password"
                                     placeholder="••••••••"
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "
                                     required=""> @error('password_confirmation')
                                 <div id="emailHelp" style="color:red" class="form-text">{{ $message }}</div>
                                 @enderror
@@ -98,20 +98,20 @@
                             <div class="flex items-start">
                                 <div class="flex items-center h-5">
                                     <input id="terms" aria-describedby="terms" type="checkbox"
-                                        class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                                        class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 "
                                         required="">
                                 </div>
                                 <div class="ml-3 text-sm">
-                                    <label for="terms" class="font-light text-gray-500 dark:text-gray-300">I accept the
-                                        <a class="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                                    <label for="terms" class="font-lighttext-blue">I accept the
+                                        <a class="font-medium text-primary-600 hover:underline "
                                             href="#">Terms and Conditions</a></label>
                                 </div>
                             </div>
                             <button type="submit"
-                                class="w-full bg-yellow text-blue font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-                                style="background:#d3d2b5">Create
+                                class="w-full bg-blue text-yellow font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                                >Create
                                 an account</button>
-                            <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                            <p class="text-sm font-light text-blue">
                                 Already have an account? <a data-modal-toggle="authentication-modal" href="#"
                                     class="font-medium text-primary-600 hover:underline dark:text-primary-500">Login
                                     here</a>
--- resources/views/reusecomp/navbar.blade.php
+++ resources/views/reusecomp/navbar.blade.php
@@ -7,25 +7,25 @@
 
 <nav class="bg-yellow  fixed w-full z-20 top-0 start-0">
   <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
-      <img src="{{ asset('assets/Images/logo.png') }}" class="h-8" alt="Flowbite Logo">
+    <a href="" class="flex items-center space-x-3 rtl:space-x-reverse">
+      <img src="{{ asset('assets/Images/logo.png') }}" class="h-8">
 
     </a>
     <div class="flex flex-row gap-[5px] md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
       @if(!Auth::user())
       <button data-modal-target="authentication-modal" data-modal-toggle="authentication-modal" type="button" id="login"
-        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</button>
+        class="text-white bg-blue focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center" style="background:#234E70">Login</button>
       @else
 
       @endif
       <!-- Dropdown menu -->
       @if (Auth::user())
       <a href="/post-campaign"
-        class=" text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Post
+        class=" text-white bg-blue hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center ">Post
         Campaign</a>
 
         <a href="/register-club"
-        class=" text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register Organization
+        class=" text-white bg-blue hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center ">Register Organization
         </a>
 
       <button id="dropdownNotificationButton" data-dropdown-toggle="dropdownNotification"
@@ -50,8 +50,8 @@
           alt="user photo">
       </button>
       <div id="dropdownAvatar"
-        class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
-        <div class="px-4 py-3 text-sm text-gray-900 dark:text-white">
+        class="z-10 hidden bg-blue divide-y divide-gray-100 rounded-lg shadow w-44 bg-gray-700 divide-gray-600">
+        <div class="px-4 py-3 text-sm text-gray-900 text-white">
           <div>{{Auth::user()->fname}}{{ Auth::user()->mname }} {{ Auth::user()->lname}}</div>
           <div class="font-medium truncate">{{Auth::user()->email}}</div>
         </div>
@@ -62,12 +62,12 @@
           </li>
           <li>
             <a href="/setting/{{Auth::user()->id}}/{{Auth::user()->fname}}"
-              class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 text-yellow">Settings</a>
+              class="block px-4 py-2  text-yellow">Settings</a>
           </li>
 
         </ul>
         <div class="py-2">
-          <a href="/logout" class="block px-4 py-2 text-sm  bg-blue dark:text-gray-200  text-yellow">Sign out</a>
+          <a href="/logout" class="block px-4 py-2 text-sm  bg-blue  text-yellow">Sign out</a>
         </div>
       </div>
       @else
@@ -78,7 +78,7 @@
 
 
       <button data-collapse-toggle="navbar-sticky" type="button"
-        class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+        class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 
         aria-controls="navbar-sticky" aria-expanded="false">
         <span class="sr-only">Open main menu</span>
         <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
@@ -91,21 +91,21 @@
       <ul
         class="flex flex-col p-4 md:p-0 mt-4 font-medium  rounded-lg  md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0  ">
         <li>
-          <a href="/" class="block py-2 px-3  rounded md:p-0" aria-current="page">Home</a>
+          <a href="/" class="block py-2 px-3 text-blue rounded md:p-0" aria-current="page">Home</a>
         </li>
         <li>
           <a href="/view-campaigns"
-            class="block py-2 px-3  rounded  md:p-0 text-blue dark:border-gray-700">Campaigns</a>
+            class="block py-2 px-3  rounded  md:p-0 text-blue">Campaigns</a>
         </li>
         <li>
           <a href="/view-campaigns"
-            class="block py-2 px-3  rounded  md:p-0 text-blue dark:border-gray-700">Social Organizations</a>
+            class="block py-2 px-3  rounded  md:p-0 text-blue">Social Organizations</a>
         </li>
         <li>
-          <a href="#" class="block py-2 px-3  rounded  md:p-0 text-blue dark:border-gray-700">About</a>
+          <a href="/about" class="block py-2 px-3  rounded  md:p-0 text-blue">About</a>
         </li>
         <li>
-          <a href="#" class="block py-2 px-3  rounded  md:p-0 text-blue dark:border-gray-700">Contact</a>
+          <a href="#" class="block py-2 px-3  rounded  md:p-0 text-blue">Contact</a>
         </li>
       </ul>
     </div>
